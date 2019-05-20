02dd317f615a8e218bcaab0faa26df52db864a17
Ann <annwam2007@yahoo.com>
2019-05-20 16:02:59 -0700

    Add Hello world message

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,8 +1,11 @@
 package org.launchcode;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World");
     }
 }
